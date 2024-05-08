5ec599022493c45579f5dfe70f6cfb3546fa7881
pranav1st <78193076+pranav1st@users.noreply.github.com>
2024-05-08 13:12:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 
 content = "Hello World"
-file_path = "/home/Desktop/hello.txt"
+file_path = "/home/Ubuntu/Desktop/hello.txt"
 
 try:
     with open(file_path, 'w') as file:
         file.write(content)
-    print("File 'hello.txt' created successfully at /home/Desktop.")
+    print("File 'hello.txt' created successfully at /home/Ubuntu/Desktop.")
 except FileNotFoundError:
-    print("Error: The specified directory '/home/Desktop' does not exist.")
+    print("Error: The specified directory '/home/Ubuntu/Desktop' does not exist.")
 except PermissionError:
-    print("Error: Permission denied to write to '/home/Desktop'.")
+    print("Error: Permission denied to write to '/home/Ubuntu/Desktop'.")
 except Exception as e:
     print("An error occurred:", e)
